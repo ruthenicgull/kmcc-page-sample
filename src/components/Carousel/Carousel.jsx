c07dd4988aa69c3f5d45f32d5cb6c409b201b171
[Muhammed Hisham] <[muhammedhisham2003@gmail.com]>
2024-05-29 18:06:53 +0300

    carousel working

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import styles from "./Carousel.module.css";
+import slides from "./slides.json";
 
 function Carousel() {
+  const [itemIndex, setItemIndex] = useState(0);
+
+  function showNextSlide() {
+    if (itemIndex === slides.length - 1) setItemIndex(0);
+    else setItemIndex((itemIndex) => itemIndex + 1);
+  }
+
+  function showPrevSlide() {
+    if (itemIndex === 0) setItemIndex(slides.length - 1);
+    else setItemIndex((itemIndex) => itemIndex - 1);
+  }
+
   return (
     <div className={styles.container}>
-      <img src="/arrow.png" className={styles.car_left} />
+      <img
+        src="/arrow.png"
+        className={styles.car_left}
+        onClick={showPrevSlide}
+      />
       <div className={styles.content}>
-        <img src="/office1.png" alt="#" className={styles.image} />
+        <img
+          src={slides[itemIndex].imageUrl}
+          alt="#"
+          className={styles.image}
+        />
         <div className={styles.content_text}>
-          <h1>Elevate your events with unforgettable experiences</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          <h1>{slides[itemIndex].title}</h1>
+          <p>{slides[itemIndex].description}</p>
           <div className={styles.button_group}>
             <button className={styles.button}>Contact</button>
             <button className={`${styles.button} ${styles.button_outline}`}>
@@ -20,7 +39,11 @@ function Carousel() {
           </div>
         </div>
       </div>
-      <img src="/arrow.png" className={styles.car_right} />
+      <img
+        src="/arrow.png"
+        className={styles.car_right}
+        onClick={showNextSlide}
+      />
     </div>
   );
 }
