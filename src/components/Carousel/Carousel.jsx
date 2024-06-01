6cbfe006e0036e20efa8f4f3295f7a824dda3627
[Muhammed Hisham] <[muhammedhisham2003@gmail.com]>
2024-06-01 19:42:44 +0300

    removed bad css

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,18 +1,29 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import styles from "./Carousel.module.css";
 import slides from "./slides.json";
 
 function Carousel() {
   const [itemIndex, setItemIndex] = useState(0);
 
+  // const autoRollEffect = useCallback(() => {
+  //   if (itemIndex >= slides.length - 1) setItemIndex(0);
+  //   else setItemIndex((prevIndex) => prevIndex + 1);
+  // }, [itemIndex]);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     autoRollEffect();
+  //   }, 3000);
+  // }, [autoRollEffect]);
+
   function showNextSlide() {
     if (itemIndex === slides.length - 1) setItemIndex(0);
-    else setItemIndex((itemIndex) => itemIndex + 1);
+    else setItemIndex((prevIndex) => prevIndex + 1);
   }
 
   function showPrevSlide() {
     if (itemIndex === 0) setItemIndex(slides.length - 1);
-    else setItemIndex((itemIndex) => itemIndex - 1);
+    else setItemIndex((prevIndex) => prevIndex - 1);
   }
 
   return (
@@ -24,13 +35,13 @@ function Carousel() {
       />
       <div className={styles.content}>
         <img
-          src={slides[itemIndex].imageUrl}
+          src={slides[itemIndex]?.imageUrl}
           alt="#"
           className={styles.image}
         />
         <div className={styles.content_text}>
-          <h1>{slides[itemIndex].title}</h1>
-          <p>{slides[itemIndex].description}</p>
+          <h1>{slides[itemIndex]?.title}</h1>
+          <p>{slides[itemIndex]?.description}</p>
           <div className={styles.button_group}>
             <button className={styles.button}>Contact</button>
             <button className={`${styles.button} ${styles.button_outline}`}>
